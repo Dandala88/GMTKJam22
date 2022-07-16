36ac2293ac6290e90cf69143dc166db829fa653d
Dan <me@danielholcomb.com>
2022-07-15 18:29:32 -0600

    Main Menu added

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField]
-    Transform[] options;
+    TextMeshProUGUI[] options;
+    [SerializeField]
+    Transform select;
+    [SerializeField]
+    Color selectedColor;
+    [SerializeField]
+    Color unselectedColor;
 
     private PlayerInput playerInput;
 
@@ -16,17 +25,23 @@ public class MainMenu : MonoBehaviour
     {
         playerInput = GetComponent<PlayerInput>();
         playerInput.SwitchCurrentActionMap("Menu");
+        SelectOption();
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        if(context.performed)
+        if(context.started)
         {
             Vector2 input = context.ReadValue<Vector2>();
             if(input.magnitude > 0)
             {
-                currentSelection = (int)Mathf.Clamp(currentSelection + input.y, 0, 1);
-                Debug.Log(currentSelection);
+                currentSelection -= (int)input.y;
+                if(currentSelection >= options.Length)
+                    currentSelection = 0;
+                else if(currentSelection < 0)
+                    currentSelection = options.Length - 1;
+                SelectOption();
+
             }
         }
     }
@@ -35,8 +50,21 @@ public class MainMenu : MonoBehaviour
     {
         if (context.started)
         {
-            Debug.Log("Yup");
+            switch(currentSelection)
+            {
+                case 0: SceneManager.LoadScene(1, LoadSceneMode.Single); break;
+                case 1: Application.Quit(); Debug.Log("Quit Game"); break;
+            }
         }
+    }
+
+    private void SelectOption()
+    {
+        select.position = options[currentSelection].transform.position;
+
+        foreach (TextMeshProUGUI tm in options)
+            tm.color = unselectedColor;
+        options[currentSelection].color = selectedColor;
     }
 
 }
