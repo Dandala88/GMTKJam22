380f882a9de998c4547cdaf35f6e9b28ce41450d
bbunks <brendenbunker@gmail.com>
2022-07-16 03:24:52 -0600

    lerp rebuild on sphere size
    Rebuilt the morph to use the lerp based on delta time. Sets target radius based on if it is going the same direction and if there is currently movement input.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,6 +14,9 @@ public class PlayerController : MonoBehaviour
     private float maxSphereColliderSize;
 
     [SerializeField]
+    private float shapeShiftSpeed;
+
+    [SerializeField]
     private float maxAngularVelocityPower;
 
     [SerializeField]
@@ -34,6 +37,12 @@ public class PlayerController : MonoBehaviour
         rb.maxAngularVelocity = 10 * (maxAngularVelocityPower * maxAngularVelocityPower);
     }
 
+    void Start()
+    {
+        //this fixes the torque for mesh colliders
+        rb.inertiaTensor = new Vector3(1, 1, 1);
+    }
+
     public void Move(Vector2 sentInput)
     {
         input = sentInput;
@@ -52,10 +61,23 @@ public class PlayerController : MonoBehaviour
             * translatedInput;
         rb.AddTorque(translatedInput * moveForce, ForceMode.Acceleration);
 
+        //Javascript people be like; Check if velocity and target direction and the same and that the key board is active.
+        //If so, the target for the lerp is set to the max size. Else it is set for the minimum size. Helps give more of the corner physics when turning around
+        float targetRadius =
+            (
+                translatedInput.magnitude > .1
+                && Vector3.Dot(
+                    new Vector3(-translatedInput.z, 0, translatedInput.x).normalized,
+                    new Vector3(rb.velocity.x, 0, rb.velocity.z).normalized
+                ) > 0
+            )
+                ? maxSphereColliderSize
+                : minSphereColliderSize;
+
         sphereCollider.radius = Mathf.Lerp(
-            minSphereColliderSize,
-            maxSphereColliderSize,
-            input.magnitude
+            sphereCollider.radius,
+            targetRadius,
+            shapeShiftSpeed * Time.deltaTime
         );
     }
 }
