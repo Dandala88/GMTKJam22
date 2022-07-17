2ba27027b3be4e760050219fd5a56c956ea3bb74
Dan <me@danielholcomb.com>
2022-07-17 08:16:24 -0600

    fix playerpref logic

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,6 +6,11 @@ public class LevelManager : MonoBehaviour
 {
     public List<float> scores;
 
+    private void Start()
+    {
+        InitializeScores();
+    }
+
     private void OnEnable()
     {
         EndGoal.OnCleared += UpdateScore;
@@ -19,9 +24,20 @@ public class LevelManager : MonoBehaviour
     private void UpdateScore(int level, float score)
     {
         string playerPrefsKey = "Level_" + level;
-        if (PlayerPrefs.GetFloat(playerPrefsKey) > score)
+        var playerPrefs = PlayerPrefs.GetFloat(playerPrefsKey);
+        if (PlayerPrefs.GetFloat(playerPrefsKey) > score || !PlayerPrefs.HasKey(playerPrefsKey))
             PlayerPrefs.SetFloat(playerPrefsKey, score);
         scores[level] = score;
+    }
+
+    private void InitializeScores()
+    {
+        int i = 0;
+        while (PlayerPrefs.HasKey("Level_" + i))
+        {
+            scores[i] = PlayerPrefs.GetFloat("Level_" + i);
+            i++;
+        }
     }
 
 }
