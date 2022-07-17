caec9b24e709a85e771e450c8a3651867c948c31
bbunks <brendenbunker@gmail.com>
2022-07-17 11:17:47 -0600

    MUSIC BB

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public AudioSource audioSource;
+    public int index;
+    public AudioClip[] songs;
+
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
+        index = 0;
+    }
+
+    private void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = songs[index++];
+
+            if (index == songs.Length)
+                index = 0;
+
+            audioSource.Play();
+        }
     }
 }
